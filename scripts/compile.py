c4c7a62daad51fac72ee3c486ec79e685bb9fbdc
Martín Triay <martriay@gmail.com>
2021-09-16 20:16:05 -0300

    add contracts directory to compilation command

--- scripts/compile.py
+++ scripts/compile.py
@@ -2,17 +2,50 @@
 import sys, os, subprocess
 from config import CONTRACTS_DIRECTORY, BUILD_DIRECTORY, ABIS_DIRECTORY
 
-def compile(path):
+
+def compile(params):
+  """
+  $ compile.py 
+    Compiles all contracts in CONTRACTS_DIRECTORY
+
+  $ compile.py contracts/MyContract.cairo
+    Compiles MyContract.cairo
+
+  $ compile.py contracts/foo.cairo contracts/bar.cairo
+    Compiles foo.cairo and bar.cairo
+  """
+
+  # to do: automatically support subdirectories
+
+  if not os.path.exists(ABIS_DIRECTORY):
+    print("Creating {} to store compilation artifacts".format(ABIS_DIRECTORY))
+    os.makedirs(ABIS_DIRECTORY, exist_ok=True)
+
+  if len(params) == 0:
+    print("🤖 Compiling all Cairo contracts in the {} directory".format(CONTRACTS_DIRECTORY))
+    for path in get_all_contracts():
+      compile_contract(path)
+  elif len(params) == 1:
+    compile_contract(params[0])
+  else:
+    for path in params[1:]:
+      compile_contract(path)
+  
+  print("✅ Done")
+
+def compile_contract(path):
   base = os.path.basename(path)
   filename = os.path.splitext(base)[0]
   print("🔨 Compiling {}".format(path))
 
   cmd = """
   starknet-compile {path} \
+    --cairo_path={CONTRACTS_DIRECTORY}
     --output {BUILD_DIRECTORY}{filename}.json \
     --abi {ABIS_DIRECTORY}{filename}.json
   """.format(
     path=path,
+    CONTRACTS_DIRECTORY=CONTRACTS_DIRECTORY,
     BUILD_DIRECTORY=BUILD_DIRECTORY,
     ABIS_DIRECTORY=ABIS_DIRECTORY,
     filename=filename)
@@ -28,18 +61,4 @@ def get_all_contracts():
 
 
 if __name__ == "__main__":
-  if not os.path.exists(ABIS_DIRECTORY):
-    print("Creating {} to store compilation artifacts".format(ABIS_DIRECTORY))
-    os.makedirs(ABIS_DIRECTORY, exist_ok=True)
-  
-  params = sys.argv
-
-  if len(params) == 1:
-    print("🤖 Compiling all Cairo contracts in the {} directory".format(CONTRACTS_DIRECTORY))
-    for path in get_all_contracts():
-      compile(path)
-  elif len(params) == 2:
-    compile(params[1])
-  else:
-    for path in params[1:]:
-      compile(path)
+  compile(sys.argv[1:])

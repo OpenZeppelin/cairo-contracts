f786f7860a7dc835412ddd8fd4acc90a04ccbae8
Andrew Fleming <fleming.andrew@protonmail.com>
2021-11-06 23:14:35 -0400

    Delete Deploy.py

--- test/utils/Deploy.py
+++
@@ -1,7 +0,0 @@
-from starkware.starknet.compiler.compile import compile_starknet_files
-
-
-async def deploy_contract(starknet, path, params=[]):
-    contract_definition = compile_starknet_files([path], debug_info=True)
-    deployed_contract = await starknet.deploy(contract_def=contract_definition, constructor_calldata=params)
-    return deployed_contract

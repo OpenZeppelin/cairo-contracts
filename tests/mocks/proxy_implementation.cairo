73c4b7e2d6d783d67cd4bccfe1933d6d433adc7a
Andrew <fleming-andrew@protonmail.com>
2022-02-25 23:30:51 -0500

    fix proxy implementation

--- tests/mocks/proxy_implementation.cairo
+++ tests/mocks/proxy_implementation.cairo
@@ -3,16 +3,11 @@
 
 from starkware.cairo.common.cairo_builtins import HashBuiltin
 from starkware.cairo.common.uint256 import Uint256
-from starkware.starknet.common.messages import send_message_to_l1
-from starkware.cairo.common.alloc import alloc
 
 from openzeppelin.upgrades.library import (
     Proxy_initializer,
     Proxy_get_implementation
 )
-
-const L1_CONTRACT_ADDRESS = 999
-const MESSAGE = 111
 
 #
 # Storage
--- tests/upgrades/test_Proxy.py
+++ tests/upgrades/test_Proxy.py
@@ -81,7 +81,16 @@ async def test_constructor_sets_correct_implementation(proxy_factory):
 
 
 @pytest.mark.asyncio
-async def test_delegate_call(proxy_factory):
+async def test_initializer(proxy_factory):
+    account, implementation, proxy = proxy_factory
+
+    await signer.send_transaction(
+        account, proxy.contract_address, 'initialize', []
+    )
+
+
+@pytest.mark.asyncio
+async def test_default_fallback(proxy_factory):
     account, _, proxy = proxy_factory
 
     # set value through proxy

25414b5593a7b704541b02c6166ffbf58bc88474
Andrew <fleming-andrew@protonmail.com>
2022-02-25 22:19:26 -0500

    simplify contract, add tests

---
+++ tests/mocks/proxy_implementation.cairo
@@ -0,0 +1,75 @@
+%lang starknet
+%builtins pedersen range_check
+
+from starkware.cairo.common.cairo_builtins import HashBuiltin
+from starkware.cairo.common.uint256 import Uint256
+from starkware.starknet.common.messages import send_message_to_l1
+from starkware.cairo.common.alloc import alloc
+
+from openzeppelin.upgrades.library import (
+    Proxy_initializer,
+    Proxy_get_implementation
+)
+
+const L1_CONTRACT_ADDRESS = 999
+const MESSAGE = 111
+
+#
+# Storage
+#
+
+@storage_var
+func value() -> (res: felt):
+end
+
+#
+# Initializer
+#
+
+@external
+func initializer{
+        syscall_ptr : felt*, 
+        pedersen_ptr : HashBuiltin*,
+        range_check_ptr
+    }(proxy_admin: felt):
+    Proxy_initializer(proxy_admin)
+    return ()
+end
+
+#
+# Getters
+#
+
+@view
+func get_value{
+        syscall_ptr : felt*, 
+        pedersen_ptr : HashBuiltin*,
+        range_check_ptr
+    }() -> (val: felt):
+    let (val) = value.read()
+    return (val)
+end
+
+@view
+func get_implementation{
+        syscall_ptr : felt*, 
+        pedersen_ptr : HashBuiltin*,
+        range_check_ptr
+    }() -> (address: felt):
+    let (address) = Proxy_get_implementation()
+    return (address)
+end
+
+#
+# Setters
+#
+
+@external
+func set_value{
+        syscall_ptr : felt*, 
+        pedersen_ptr : HashBuiltin*,
+        range_check_ptr
+    }(val: felt):
+    value.write(val)
+    return ()
+end
---
+++ tests/upgrades/test_Proxy.py
@@ -0,0 +1,110 @@
+import pytest
+import asyncio
+from starkware.starknet.testing.starknet import Starknet
+from starkware.starkware_utils.error_handling import StarkException
+from starkware.starknet.definitions.error_codes import StarknetErrorCode
+from utils import (
+    Signer, assert_revert, assert_event_emitted, get_contract_def, cached_contract
+)
+
+# random value
+VALUE = 123
+
+
+signer = Signer(123456789987654321)
+
+
+@pytest.fixture(scope='module')
+def event_loop():
+    return asyncio.new_event_loop()
+
+
+@pytest.fixture(scope='module')
+def contract_defs():
+    account_def = get_contract_def('openzeppelin/account/Account.cairo')
+    implementation_def = get_contract_def(
+        'tests/mocks/proxy_implementation.cairo'
+    )
+    proxy_def = get_contract_def('openzeppelin/upgrades/Proxy.cairo')
+
+    return account_def, implementation_def, proxy_def
+
+
+@pytest.fixture(scope='module')
+async def proxy_init(contract_defs):
+    account_def, implementation_def, proxy_def = contract_defs
+    starknet = await Starknet.empty()
+    account = await starknet.deploy(
+        contract_def=account_def,
+        constructor_calldata=[signer.public_key]
+    )
+    implementation = await starknet.deploy(
+        contract_def=implementation_def,
+        constructor_calldata=[]
+    )
+    proxy = await starknet.deploy(
+        contract_def=proxy_def,
+        constructor_calldata=[implementation.contract_address]
+    )
+    return (
+        starknet.state,
+        account,
+        implementation,
+        proxy
+    )
+
+
+@pytest.fixture
+def proxy_factory(contract_defs, proxy_init):
+    account_def, implementation_def, proxy_def = contract_defs
+    state, account, implementation, proxy = proxy_init
+    _state = state.copy()
+    account = cached_contract(_state, account_def, account)
+    implementation = cached_contract(
+        _state,
+        implementation_def,
+        implementation
+    )
+    proxy = cached_contract(_state, proxy_def, proxy)
+
+    return account, implementation, proxy
+
+
+@pytest.mark.asyncio
+async def test_constructor_sets_correct_implementation(proxy_factory):
+    account, implementation, proxy = proxy_factory
+
+    execution_info = await signer.send_transaction(
+        account, proxy.contract_address, 'get_implementation', []
+    )
+    assert execution_info.result.response == [implementation.contract_address]
+
+
+@pytest.mark.asyncio
+async def test_delegate_call(proxy_factory):
+    account, _, proxy = proxy_factory
+
+    # set value through proxy
+    await signer.send_transaction(
+        account, proxy.contract_address, 'set_value', [VALUE]
+    )
+
+    # get value through proxy
+    execution_info = execution_info = await signer.send_transaction(
+        account, proxy.contract_address, 'get_value', []
+    )
+    assert execution_info.result.response == [VALUE]
+
+
+@pytest.mark.asyncio
+async def test_delegate_call_when_selector_does_not_exist(proxy_factory):
+    account, _, proxy = proxy_factory
+
+    try:
+        await signer.send_transaction(
+            account, proxy.contract_address, 'bad_selector', []
+        )
+        assert False
+    except StarkException as err:
+        _, error = err.args
+        assert error['code'] == StarknetErrorCode.ENTRY_POINT_NOT_FOUND_IN_CONTRACT

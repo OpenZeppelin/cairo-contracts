ecf93f9964b894b9d11d48f4d65e31e18c8751cb
Andrew <fleming-andrew@protonmail.com>
2022-03-21 02:15:17 -0400

    fix hex type errors in signer

--- tests/utilities/SignerWithAccount.py
+++ tests/utilities/SignerWithAccount.py
@@ -43,5 +43,11 @@ class SignerWithAccount():
             execution_info = await account.get_nonce().call()
             nonce, = execution_info.result
 
-        (call_array, calldata, sig_r, sig_s) = self.signer.sign_transaction(account.contract_address, calls, nonce, max_fee)
+        build_calls = []
+        for call in calls:
+            build_call = list(call)
+            build_call[0] = hex(build_call[0])
+            build_calls.append(build_call)
+
+        (call_array, calldata, sig_r, sig_s) = self.signer.sign_transaction(hex(account.contract_address), build_calls, nonce, max_fee)
         return await account.__execute__(call_array, calldata, nonce).invoke(signature=[sig_r, sig_s])

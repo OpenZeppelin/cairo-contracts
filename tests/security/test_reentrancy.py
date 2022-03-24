a77b26c032baa4cd20cf94bc5accaf201a5fe92c
Julissa Dantes <julissadcj@gmail.com>
2022-03-24 15:15:20 -0400

    Update reentrancy test

--- tests/security/test_reentrancy.py
+++ tests/security/test_reentrancy.py
@@ -2,22 +2,28 @@ import pytest
 from starkware.starknet.testing.starknet import Starknet
 from utils import TRUE, FALSE, assert_revert
 
+INITIAL_COUNTER = 0
 
 @pytest.mark.asyncio
-async def test_initializer():
+async def test_reentrancy_guard():
     starknet = await Starknet.empty()
-    reentrancy_guard = await starknet.deploy("openzeppelin/security/reentrancy_guard.cairo")
-    expected = await reentrancy_guard.initialized().call()
-    assert expected.result == (FALSE,)
+    contract = await starknet.deploy("tests/mocks/reentrancy_mock.cairo", constructor_calldata=[INITIAL_COUNTER])
+    attacker = await starknet.deploy("tests/mocks/reentrancy_attacker_mock.cairo")
+    response = await contract.current_count().call()
 
-    await reentrancy_guard.reentrancyGuard_start().invoke()
-
-    expected = await reentrancy_guard.reentrancyGuard_start().call()
-    assert expected.result == (TRUE,)
+    assert response.result == (INITIAL_COUNTER,)
     # should not allow remote callback
+    await assert_revert(
+        contract.countAndCall(attacker.contract_address).invoke(),
+        reverted_with="ReentrancyGuard: reentrant call"
+    )
     # should not allow local recursion
+    await assert_revert(
+        contract.countLocalRecursive(10).invoke(),
+        reverted_with="ReentrancyGuard: reentrant call"
+    )
     # should not allow indirect local recursion
     await assert_revert(
-        reentrancy_guard.reentrancyGuard_start().invoke(),
+        contract.countThisRecursive(10).invoke(),
         reverted_with="ReentrancyGuard: reentrant call"
     )

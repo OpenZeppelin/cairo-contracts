574bc03474ff6deba186dbce3e17ffa62f7d7286
Julien Niset <julien.niset@gmail.com>
2022-03-25 23:31:37 +0100

    use `tx_info.transaction_hash` instead of `hash_multicall` in Account (#233)
    * use `tx_info.transaction_hash` instead of `hash_multicall`
    
    * fix tests

--- src/openzeppelin/utils/constants.cairo
+++ src/openzeppelin/utils/constants.cairo
@@ -11,12 +11,6 @@ const TRUE = 1
 const FALSE = 0
 
 #
-# Hashing Transactions
-#
-
-const PREFIX_TRANSACTION = 'StarkNet Transaction'
-
-#
 # Numbers
 #
 
--- tests/utils.py
+++ tests/utils.py
@@ -9,7 +9,8 @@ from starkware.starknet.compiler.compile import compile_starknet_files
 from starkware.starkware_utils.error_handling import StarkException
 from starkware.starknet.testing.starknet import StarknetContract
 from starkware.starknet.business_logic.transaction_execution_objects import Event
-
+from starkware.starknet.core.os.transaction_hash import calculate_transaction_hash_common, TransactionHashPrefix
+from starkware.starknet.definitions.general_config import StarknetChainId
 
 MAX_UINT256 = (2**128 - 1, 2**128 - 1)
 INVALID_UINT256 = (MAX_UINT256[0] + 1, MAX_UINT256[1])
@@ -170,8 +171,7 @@ class Signer():
             (call[0], get_selector_from_name(call[1]), call[2]) for call in calls]
         (call_array, calldata) = from_call_to_call_array(calls)
 
-        message_hash = hash_multicall(
-            account.contract_address, calls_with_selector, nonce, max_fee)
+        message_hash = get_transaction_hash(account.contract_address, call_array, calldata, nonce, max_fee)
         sig_r, sig_s = self.sign(message_hash)
 
         return await account.__execute__(call_array, calldata, nonce).invoke(signature=[sig_r, sig_s])
@@ -188,19 +188,21 @@ def from_call_to_call_array(calls):
         calldata.extend(call[2])
     return (call_array, calldata)
 
+def get_transaction_hash(account, call_array, calldata, nonce, max_fee):
+    execute_calldata = [
+        len(call_array),
+        *[x for t in call_array for x in t],
+        len(calldata),
+        *calldata,
+        nonce]
 
-def hash_multicall(sender, calls, nonce, max_fee):
-    hash_array = []
-    for call in calls:
-        call_elements = [call[0], call[1], compute_hash_on_elements(call[2])]
-        hash_array.append(compute_hash_on_elements(call_elements))
-
-    message = [
-        str_to_felt('StarkNet Transaction'),
-        sender,
-        compute_hash_on_elements(hash_array),
-        nonce,
+    return calculate_transaction_hash_common(
+        TransactionHashPrefix.INVOKE,
+        TRANSACTION_VERSION,
+        account,
+        get_selector_from_name('__execute__'),
+        execute_calldata,
         max_fee,
-        TRANSACTION_VERSION
-    ]
-    return compute_hash_on_elements(message)
+        StarknetChainId.TESTNET.value,
+        []
+    )

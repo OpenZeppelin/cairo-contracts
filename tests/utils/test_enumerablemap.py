795637af099ea0efac40052b077fc4f30275dd0b
koloz <zach.kolodny@gmail.com>
2022-05-02 23:37:17 -0400

    changed send txn to invoke

--- tests/utils/test_enumerablemap.py
+++ tests/utils/test_enumerablemap.py
@@ -1,100 +1,123 @@
 import pytest
-import asyncio
 from starkware.starknet.testing.starknet import Starknet
 from starkware.starkware_utils.error_handling import StarkException
 from starkware.starknet.definitions.error_codes import StarknetErrorCode
-from utils import Signer, assert_revert, contract_path
+from utils import assert_revert, contract_path, TRUE, FALSE
 
-signer = Signer(123456789987654321)
-
-TRUE = 1
-FALSE = 0
-
-
-@pytest.fixture(scope='module')
-def event_loop():
-    return asyncio.new_event_loop()
-
-
-@pytest.fixture(scope='module')
+@pytest.fixture
 async def enumerablemap_mock():
     starknet = await Starknet.empty()
-
-    account = await starknet.deploy(
-        contract_path("openzeppelin/account/Account.cairo"),
-        constructor_calldata=[signer.public_key]
-    )
 
     enumerablemap = await starknet.deploy(
         contract_path("tests/mocks/enumerablemap_mock.cairo")
     )
 
-    return account, enumerablemap
+    return enumerablemap
 
 
 @pytest.mark.asyncio
 async def test_set(enumerablemap_mock):
-    account, enumerablemap = enumerablemap_mock
-    await signer.send_transaction(account, enumerablemap.contract_address, 'set', [6, 8])
+    enumerablemap = enumerablemap_mock
+
+    success = await enumerablemap.set(6, 8).invoke()
+    assert success.result.success == TRUE
+
     executed_info = await enumerablemap.contains(6).call()
     assert executed_info.result == (TRUE,)
 
 
 @pytest.mark.asyncio
 async def test_remove(enumerablemap_mock):
-    account, enumerablemap = enumerablemap_mock
-    await signer.send_transaction(account, enumerablemap.contract_address, 'remove', [6])
+    enumerablemap = enumerablemap_mock
+
+    success = await enumerablemap.set(6, 8).invoke()
+    assert success.result.success == TRUE
+
+    success = await enumerablemap.remove(6).invoke()
+    assert success.result.success == TRUE
+
+
+    executed_info = await enumerablemap.contains(6).call()
+    assert executed_info.result == (FALSE,)
+
+
+@pytest.mark.asyncio
+async def test_remove_fail(enumerablemap_mock):
+    enumerablemap = enumerablemap_mock
+
+    success = await enumerablemap.remove(6).invoke()
+    assert success.result.success == FALSE
+
+
     executed_info = await enumerablemap.contains(6).call()
     assert executed_info.result == (FALSE,)
 
 
 @pytest.mark.asyncio
 async def test_update(enumerablemap_mock):
-    account, enumerablemap = enumerablemap_mock
-    await signer.send_transaction(account, enumerablemap.contract_address, 'set', [6, 8])
+    enumerablemap = enumerablemap_mock
+
+    success = await enumerablemap.set(6, 8).invoke()
+    assert success.result.success == TRUE
+    
     executed_info = await enumerablemap.get(6).call()
     assert executed_info.result == (TRUE, 8,)
-    await signer.send_transaction(account, enumerablemap.contract_address, 'set', [6, 19])
+
+    success = await enumerablemap.set(6, 19).invoke()
+    assert success.result.success == FALSE
+
     executed_info = await enumerablemap.get(6).call()
     assert executed_info.result == (TRUE, 19,)
+
     executed_info = await enumerablemap.length().call()
     assert executed_info.result == (1,)
 
 
 @pytest.mark.asyncio
 async def test_two_sets(enumerablemap_mock):
-    account, enumerablemap = enumerablemap_mock
-    await signer.send_transaction(account, enumerablemap.contract_address, 'set', [6, 8])
-    await signer.send_transaction(account, enumerablemap.contract_address, 'set', [8, 19])
+    enumerablemap = enumerablemap_mock
+
+    success = await enumerablemap.set(6, 8).invoke()
+    assert success.result.success == TRUE
+
+    success = await enumerablemap.set(8, 19).invoke()
+    assert success.result.success == TRUE
+
     executed_info = await enumerablemap.get(6).call()
     assert executed_info.result == (TRUE, 8,)
+
     executed_info = await enumerablemap.get(8).call()
     assert executed_info.result == (TRUE, 19,)
+
     executed_info = await enumerablemap.length().call()
     assert executed_info.result == (2,)
 
 
 @pytest.mark.asyncio
 async def test_get(enumerablemap_mock):
-    account, enumerablemap = enumerablemap_mock
-    
-    try:
-        executed_info = await enumerablemap.get(1).call()
-        assert False
-    except StarkException as err:
-        _, error = err.args
-        assert error['code'] == StarknetErrorCode.TRANSACTION_FAILED
+    enumerablemap = enumerablemap_mock
 
-    await signer.send_transaction(account, enumerablemap.contract_address, 'set', [1, 3])
+    await assert_revert(
+        enumerablemap.get(1).call(),
+        reverted_with="EnumerableMap: nonexistent key"
+    )
+
+    success = await enumerablemap.set(1, 3).invoke()
+    assert success.result.success == TRUE
+
     executed_info = await enumerablemap.get(1).call()
     assert executed_info.result == (TRUE,3,)
 
 
 @pytest.mark.asyncio
 async def test_try_get(enumerablemap_mock):
-    account, enumerablemap = enumerablemap_mock
+    enumerablemap = enumerablemap_mock
+
     executed_info = await enumerablemap.try_get(11).call()
     assert executed_info.result == (FALSE,0,)
-    await signer.send_transaction(account, enumerablemap.contract_address, 'set', [11, 19])
+
+    success = await enumerablemap.set(11, 19).invoke()
+    assert success.result.success == TRUE
+
     executed_info = await enumerablemap.try_get(11).call()
     assert executed_info.result == (TRUE,19,)

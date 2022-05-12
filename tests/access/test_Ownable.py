11d96f833193ae31b454c13b6510815558e3fe74
JulissaDantes <julissadcj@gmail.com>
2022-05-12 16:13:16 +0000

    Update ownable_factory

--- tests/access/test_Ownable.py
+++ tests/access/test_Ownable.py
@@ -5,6 +5,7 @@ from utils import (
     TestSigner, ZERO_ADDRESS,
     assert_event_emitted,
     get_contract_def, contract_path
+    , cached_contract
 )
 
 
@@ -13,54 +14,45 @@ signer = TestSigner(123456789987654321)
 
 @pytest.fixture(scope='module')
 def contract_defs():
-    return {
-        Path(key).stem: get_contract_def(key)
-        for key in [
-            'openzeppelin/account/Account.cairo',
-            'tests/mocks/Ownable.cairo',
-        ]
-    }
+    account_def = get_contract_def('openzeppelin/account/Account.cairo')
+    ownable_def = get_contract_def(
+        'tests/mocks/Ownable.cairo')
 
+    return account_def, ownable_def
 
 @pytest.fixture(scope='module')
 async def ownable_init(contract_defs):
+    account_def, ownable_def = contract_defs
     starknet = await Starknet.empty()
     owner = await starknet.deploy(
-        contract_def=contract_defs['Account'],
+        contract_def=account_def,
         constructor_calldata=[signer.public_key]
     )
     ownable = await starknet.deploy(
-        contract_def=contract_defs['Ownable'],
+        contract_def=ownable_def,
         constructor_calldata=[owner.contract_address]
     )
     return starknet.state, ownable, owner
 
-
-@pytest.fixture(scope='module')
-async def ownable_factory():
-    starknet = await Starknet.empty()
-    owner = await starknet.deploy(
-        contract_path("openzeppelin/account/Account.cairo"),
-        constructor_calldata=[signer.public_key]
-    )
-
-    ownable = await starknet.deploy(
-        contract_path("tests/mocks/Ownable.cairo"),
-        constructor_calldata=[owner.contract_address]
-    )
-    return starknet, ownable, owner
-
-
+@pytest.fixture
+def ownable_factory(contract_defs, ownable_init):
+    account_def, ownable_def = contract_defs
+    state, ownable, owner = ownable_init
+    _state = state.copy()
+    owner = cached_contract(_state, account_def, owner)
+    ownable = cached_contract(_state, ownable_def, ownable)
+    return ownable, owner
+    
 @pytest.mark.asyncio
 async def test_constructor(ownable_factory):
-    _, ownable, owner = ownable_factory
+    ownable, owner = ownable_factory
     expected = await ownable.owner().call()
     assert expected.result.owner == owner.contract_address
 
 
 @pytest.mark.asyncio
 async def test_transferOwnership(ownable_factory):
-    _, ownable, owner = ownable_factory
+    ownable, owner = ownable_factory
     newOwner = 123
     await signer.send_transaction(owner, ownable.contract_address, 'transferOwnership', [newOwner])
     executed_info = await ownable.owner().call()
@@ -69,7 +61,7 @@ async def test_transferOwnership(ownable_factory):
 
 @pytest.mark.asyncio
 async def test_transferOwnership_emits_event(ownable_factory):
-    _, ownable, owner = ownable_factory
+    ownable, owner = ownable_factory
     newOwner = 123
     tx_exec_info = await signer.send_transaction(owner, ownable.contract_address, 'transferOwnership', [newOwner])
 
@@ -86,7 +78,7 @@ async def test_transferOwnership_emits_event(ownable_factory):
 
 @pytest.mark.asyncio
 async def test_renounceOwnership(ownable_factory):
-    _, ownable, owner = ownable_factory
+    ownable, owner = ownable_factory
     await signer.send_transaction(owner, ownable.contract_address, 'renounceOwnership', [])
     executed_info = await ownable.owner().call()
     assert executed_info.result == (ZERO_ADDRESS,)
@@ -94,7 +86,7 @@ async def test_renounceOwnership(ownable_factory):
 
 @pytest.mark.asyncio
 async def test_renounceOwnership_emits_event(ownable_factory):
-    _, ownable, owner = ownable_factory
+    ownable, owner = ownable_factory
     tx_exec_info = await signer.send_transaction(owner, ownable.contract_address, 'renounceOwnership', [])
 
     assert_event_emitted(

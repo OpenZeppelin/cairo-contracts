fa42ab36c5af63bed89ab91b761d7f3bbb3eb0a5
Andrew Fleming <fleming-andrew@protonmail.com>
2022-05-31 10:48:57 -0400

    Fix Account reentrant signature reutilization bug (#347)

--- tests/account/test_Account.py
+++ tests/account/test_Account.py
@@ -2,34 +2,70 @@ import pytest
 from starkware.starknet.testing.starknet import Starknet
 from starkware.starkware_utils.error_handling import StarkException
 from starkware.starknet.definitions.error_codes import StarknetErrorCode
-from utils import TestSigner, assert_revert, contract_path
+from utils import TestSigner, assert_revert, get_contract_def, cached_contract, TRUE
 
 
 signer = TestSigner(123456789987654321)
 other = TestSigner(987654321123456789)
 
 IACCOUNT_ID = 0xf10dbd44
-TRUE = 1
 
 
 @pytest.fixture(scope='module')
-async def account_factory():
+def contract_defs():
+    account_def = get_contract_def('openzeppelin/account/Account.cairo')
+    init_def = get_contract_def("tests/mocks/Initializable.cairo")
+    attacker_def = get_contract_def("tests/mocks/account_reentrancy.cairo")
+
+    return account_def, init_def, attacker_def
+
+
+@pytest.fixture(scope='module')
+async def account_init(contract_defs):
+    account_def, init_def, attacker_def = contract_defs
     starknet = await Starknet.empty()
-    account = await starknet.deploy(
-        contract_path("openzeppelin/account/Account.cairo"),
+
+    account1 = await starknet.deploy(
+        contract_def=account_def,
         constructor_calldata=[signer.public_key]
     )
-    bad_account = await starknet.deploy(
-        contract_path("openzeppelin/account/Account.cairo"),
-        constructor_calldata=[signer.public_key],
+    account2 = await starknet.deploy(
+        contract_def=account_def,
+        constructor_calldata=[signer.public_key]
+    )
+    initializable1 = await starknet.deploy(
+        contract_def=init_def,
+        constructor_calldata=[],
+    )
+    initializable2 = await starknet.deploy(
+        contract_def=init_def,
+        constructor_calldata=[],
+    )
+    attacker = await starknet.deploy(
+        contract_def=attacker_def,
+        constructor_calldata=[],
     )
 
-    return starknet, account, bad_account
+    return starknet.state, account1, account2, initializable1, initializable2, attacker
+
+
+@pytest.fixture
+def account_factory(contract_defs, account_init):
+    account_def, init_def, attacker_def = contract_defs
+    state, account1, account2, initializable1, initializable2, attacker = account_init
+    _state = state.copy()
+    account1 = cached_contract(_state, account_def, account1)
+    account2 = cached_contract(_state, account_def, account2)
+    initializable1 = cached_contract(_state, init_def, initializable1)
+    initializable2 = cached_contract(_state, init_def, initializable2)
+    attacker = cached_contract(_state, attacker_def, attacker)
+
+    return account1, account2, initializable1, initializable2, attacker
 
 
 @pytest.mark.asyncio
 async def test_constructor(account_factory):
-    _, account, _ = account_factory
+    account, *_ = account_factory
 
     execution_info = await account.get_public_key().call()
     assert execution_info.result == (signer.public_key,)
@@ -40,10 +76,7 @@ async def test_constructor(account_factory):
 
 @pytest.mark.asyncio
 async def test_execute(account_factory):
-    starknet, account, _ = account_factory
-    initializable = await starknet.deploy(
-        contract_path("tests/mocks/Initializable.cairo")
-    )
+    account, _, initializable, *_ = account_factory
 
     execution_info = await initializable.initialized().call()
     assert execution_info.result == (0,)
@@ -56,13 +89,7 @@ async def test_execute(account_factory):
 
 @pytest.mark.asyncio
 async def test_multicall(account_factory):
-    starknet, account, _ = account_factory
-    initializable_1 = await starknet.deploy(
-        contract_path("tests/mocks/Initializable.cairo")
-    )
-    initializable_2 = await starknet.deploy(
-        contract_path("tests/mocks/Initializable.cairo")
-    )
+    account, _, initializable_1, initializable_2, _ = account_factory
 
     execution_info = await initializable_1.initialized().call()
     assert execution_info.result == (0,)
@@ -85,10 +112,7 @@ async def test_multicall(account_factory):
 
 @pytest.mark.asyncio
 async def test_return_value(account_factory):
-    starknet, account, _ = account_factory
-    initializable = await starknet.deploy(
-        contract_path("tests/mocks/Initializable.cairo")
-    )
+    account, _, initializable, *_ = account_factory
 
     # initialize, set `initialized = 1`
     await signer.send_transactions(account, [(initializable.contract_address, 'initialize', [])])
@@ -101,10 +125,8 @@ async def test_return_value(account_factory):
 
 @ pytest.mark.asyncio
 async def test_nonce(account_factory):
-    starknet, account, _ = account_factory
-    initializable = await starknet.deploy(
-        contract_path("tests/mocks/Initializable.cairo")
-    )
+    account, _, initializable, *_ = account_factory
+
     execution_info = await account.get_nonce().call()
     current_nonce = execution_info.result.res
 
@@ -132,7 +154,7 @@ async def test_nonce(account_factory):
 
 @pytest.mark.asyncio
 async def test_public_key_setter(account_factory):
-    _, account, _ = account_factory
+    account, *_ = account_factory
 
     execution_info = await account.get_public_key().call()
     assert execution_info.result == (signer.public_key,)
@@ -146,7 +168,7 @@ async def test_public_key_setter(account_factory):
 
 @pytest.mark.asyncio
 async def test_public_key_setter_different_account(account_factory):
-    _, account, bad_account = account_factory
+    account, bad_account, *_ = account_factory
 
     # set new pubkey
     await assert_revert(
@@ -156,3 +178,16 @@ async def test_public_key_setter_different_account(account_factory):
         ),
         reverted_with="Account: caller is not this account"
     )
+
+
+@pytest.mark.asyncio
+async def test_account_takeover_with_reentrant_call(account_factory):
+    account, _, _, _, attacker = account_factory
+
+    await assert_revert(
+        signer.send_transaction(account, attacker.contract_address, 'account_takeover', []),
+        reverted_with="Account: no reentrant call"
+    )
+    
+    execution_info = await account.get_public_key().call()
+    assert execution_info.result == (signer.public_key,)

5edc9f9532e11299eaf4f57de68a1ea8af7e7af5
ctrlc03 <alecara96@liber.it>
2022-06-02 12:11:40 -0700

    Adhere to namespace patter and adding tests

--- src/openzeppelin/access/ownable.cairo
+++ src/openzeppelin/access/ownable.cairo
@@ -15,12 +15,24 @@ from starkware.cairo.common.math import assert_not_zero
 func OwnershipTransferred(previousOwner: felt, newOwner: felt):
 end
 
+@event
+func OwnershipProposed(currentOwner: felt, proposedOwner: felt):
+end
+
+@event
+func OwnershipProposalCancelled(caller: felt, proposedOwner: felt):
+end
+
 #
 # Storage
 #
 
 @storage_var
 func Ownable_owner() -> (owner: felt):
+end
+
+@storage_var
+func Ownable_proposed_owner() -> (proposed_owner: felt):
 end
 
 namespace Ownable:
@@ -33,6 +45,9 @@ namespace Ownable:
             pedersen_ptr : HashBuiltin*,
             range_check_ptr
         }(owner: felt):
+        with_attr error_message("Ownable: owner cannot be the zero address"):
+            assert_not_zero(owner)
+        end
         _transfer_ownership(owner)
         return ()
     end
@@ -67,6 +82,15 @@ namespace Ownable:
         return (owner=owner)
     end
 
+    func proposed_owner{
+            syscall_ptr : felt*,
+            pedersen_ptr : HashBuiltin*,
+            range_check_ptr
+        }() -> (proposed_owner: felt):
+        let (proposed_owner) = Ownable_proposed_owner.read()
+        return (proposed_owner=proposed_owner)
+    end
+
     func transfer_ownership{
             syscall_ptr : felt*,
             pedersen_ptr : HashBuiltin*,
@@ -78,6 +102,73 @@ namespace Ownable:
         assert_only_owner()
         _transfer_ownership(new_owner)
         return ()
+    end
+
+    func propose_owner{
+            syscall_ptr : felt*,
+            pedersen_ptr : HashBuiltin*,
+            range_check_ptr
+        }(proposed_owner: felt):
+        with_attr error_message("Ownable: proposed owner cannot be the zero address"):
+            assert_not_zero(proposed_owner)
+        end
+
+        _propose_owner(proposed_owner)
+        return()
+    end
+
+    func accept_ownership{
+            syscall_ptr : felt*,
+            pedersen_ptr : HashBuiltin*,
+            range_check_ptr
+        }():
+        let (proposed_owner) = Ownable.proposed_owner()
+        let (caller) = get_caller_address()
+        # caller cannot be zero address to avoid overwriting owner when proposed_owner is not set
+        with_attr error_message("Ownable: caller is the zero address"):
+            assert_not_zero(caller)
+        end
+
+        with_attr error_message("Ownable: caller is not the proposed owner"):
+            assert caller = proposed_owner
+        end
+
+        _transfer_ownership(proposed_owner)
+        return()
+    end
+
+    func cancel_ownership_proposal{
+            syscall_ptr : felt*,
+            pedersen_ptr : HashBuiltin*,
+            range_check_ptr
+        }():
+        alloc_locals
+        let (local proposed_owner) = Ownable.proposed_owner()
+
+        with_attr error_message("Ownable: proposed owner is already the address zero, no need to cancel"):
+            assert_not_zero(proposed_owner)
+        end
+
+        let (local current_owner) = Ownable.owner()
+        let (local caller) = get_caller_address()
+
+        tempvar syscall_ptr = syscall_ptr
+        tempvar pedersen_ptr = pedersen_ptr
+        tempvar range_check_ptr = range_check_ptr
+
+        # Can only be called by current owner or proposed owner
+        with_attr error_message("Ownable: caller is neither the current owner nor the proposed owner"):
+            if caller != proposed_owner:
+                if caller != current_owner:
+                    assert 1 = 0
+                end
+            end
+        end
+
+        Ownable_proposed_owner.write(0)
+        # Emit event for cancellation with user who cancelled and the now old proposed owner
+        OwnershipProposalCancelled.emit(caller, proposed_owner)
+        return()
     end
 
     func renounce_ownership{
@@ -105,4 +196,14 @@ namespace Ownable:
         return ()
     end
 
+    func _propose_owner{
+            syscall_ptr : felt*,
+            pedersen_ptr : HashBuiltin*,
+            range_check_ptr
+        }(proposed_owner: felt):
+        let (current_owner) = Ownable.owner()
+        Ownable_proposed_owner.write(proposed_owner)
+        OwnershipProposed.emit(current_owner, proposed_owner)
+        return ()
+    end
 end
--- tests/access/test_Ownable.py
+++ tests/access/test_Ownable.py
@@ -10,7 +10,7 @@ from utils import (
 
 
 signer = TestSigner(123456789987654321)
-
+second_signer = TestSigner(98765432123456789)
 
 @pytest.fixture(scope='module')
 def contract_defs():
@@ -28,42 +28,92 @@ async def ownable_init(contract_defs):
         contract_def=account_def,
         constructor_calldata=[signer.public_key]
     )
+    proposed_owner = await starknet.deploy(
+        contract_def=account_def,
+        constructor_calldata=[second_signer.public_key]
+    )
     ownable = await starknet.deploy(
         contract_def=ownable_def,
         constructor_calldata=[owner.contract_address]
     )
-    return starknet.state, ownable, owner
+    return starknet.state, ownable, owner, proposed_owner
 
 
 @pytest.fixture
 def ownable_factory(contract_defs, ownable_init):
     account_def, ownable_def = contract_defs
-    state, ownable, owner = ownable_init
+    state, ownable, owner, proposed_owner = ownable_init
     _state = state.copy()
     owner = cached_contract(_state, account_def, owner)
+    proposed_owner = cached_contract(_state, account_def, proposed_owner)
     ownable = cached_contract(_state, ownable_def, ownable)
-    return ownable, owner
+    return ownable, owner, proposed_owner
 
 
 @pytest.mark.asyncio
 async def test_constructor(ownable_factory):
-    ownable, owner = ownable_factory
+    ownable, owner, _ = ownable_factory
     expected = await ownable.owner().call()
     assert expected.result.owner == owner.contract_address
 
 
 @pytest.mark.asyncio
 async def test_transferOwnership(ownable_factory):
-    ownable, owner = ownable_factory
+    ownable, owner, _ = ownable_factory
     new_owner = 123
+
     await signer.send_transaction(owner, ownable.contract_address, 'transferOwnership', [new_owner])
+
     executed_info = await ownable.owner().call()
     assert executed_info.result == (new_owner,)
 
+@pytest.mark.asyncio
+async def test_proposeOwnerhsip(ownable_factory):
+    ownable, owner, proposed_owner = ownable_factory
+
+    await signer.send_transaction(owner, ownable.contract_address, 'proposeOwner', [proposed_owner.contract_address])
+    executed_info = await ownable.proposedOwner().call()
+    assert executed_info.result == (proposed_owner.contract_address, )
+
+@pytest.mark.asyncio
+async def test_CancelOwnershipProposalByOwner(ownable_factory):
+    ownable, owner, proposed_owner = ownable_factory
+
+    await signer.send_transaction(owner, ownable.contract_address, 'proposeOwner', [proposed_owner.contract_address])
+    await signer.send_transaction(owner, ownable.contract_address, 'cancelOwnershipProposal', [])
+
+    executed_info = await ownable.proposedOwner().call()
+    assert executed_info.result == (ZERO_ADDRESS, )
+
+@pytest.mark.asyncio
+async def test_CancelOwnershipProposalByProposedOwner(ownable_factory):
+    ownable, owner, proposed_owner = ownable_factory
+
+    await signer.send_transaction(owner, ownable.contract_address, 'proposeOwner', [proposed_owner.contract_address])
+    await second_signer.send_transaction(proposed_owner, ownable.contract_address, 'cancelOwnershipProposal', [])
+
+    executed_info = await ownable.proposedOwner().call()
+    assert executed_info.result == (ZERO_ADDRESS, )
+
+@pytest.mark.asyncio
+async def test_cancelOwnershipProposal_emits_event(ownable_factory):
+    ownable, owner, proposed_owner = ownable_factory
+    await signer.send_transaction(owner, ownable.contract_address, 'proposeOwner', [proposed_owner.contract_address])
+    tx_exec_info = await signer.send_transaction(owner, ownable.contract_address, 'cancelOwnershipProposal', [])
+
+    assert_event_emitted(
+        tx_exec_info,
+        from_address=ownable.contract_address,
+        name='OwnershipProposalCancelled',
+        data=[
+            owner.contract_address,
+            proposed_owner.contract_address
+        ]
+    )
 
 @pytest.mark.asyncio
 async def test_transferOwnership_emits_event(ownable_factory):
-    ownable, owner = ownable_factory
+    ownable, owner, _ = ownable_factory
     new_owner = 123
     tx_exec_info = await signer.send_transaction(owner, ownable.contract_address, 'transferOwnership', [new_owner])
 
@@ -77,10 +127,24 @@ async def test_transferOwnership_emits_event(ownable_factory):
         ]
     )
 
+@pytest.mark.asyncio
+async def test_proposeOwner_emits_event(ownable_factory):
+    ownable, owner, proposed_owner = ownable_factory
+    tx_exec_info = await signer.send_transaction(owner, ownable.contract_address, 'proposeOwner', [proposed_owner.contract_address])
+
+    assert_event_emitted(
+        tx_exec_info,
+        from_address=ownable.contract_address,
+        name='OwnershipProposed',
+        data=[
+            owner.contract_address,
+            proposed_owner.contract_address
+        ]
+    )
 
 @pytest.mark.asyncio
 async def test_renounceOwnership(ownable_factory):
-    ownable, owner = ownable_factory
+    ownable, owner, _ = ownable_factory
     await signer.send_transaction(owner, ownable.contract_address, 'renounceOwnership', [])
     executed_info = await ownable.owner().call()
     assert executed_info.result == (ZERO_ADDRESS,)
@@ -88,7 +152,7 @@ async def test_renounceOwnership(ownable_factory):
 
 @pytest.mark.asyncio
 async def test_renounceOwnership_emits_event(ownable_factory):
-    ownable, owner = ownable_factory
+    ownable, owner, _ = ownable_factory
     tx_exec_info = await signer.send_transaction(owner, ownable.contract_address, 'renounceOwnership', [])
 
     assert_event_emitted(

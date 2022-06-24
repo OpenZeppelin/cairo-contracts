28a1e6fabe78d00f7cc646fbb6ed27c44a7841c5
koloz <zach.kolodny@gmail.com>
2022-06-24 15:32:05 -0400

    added new fixuture and parameterized tests

--- tests/utils/test_enumerableset.py
+++ tests/utils/test_enumerableset.py
@@ -14,12 +14,19 @@ async def enumerableset_mock():
     return enumerableset
 
 
+@pytest.fixture
+async def enumerableset_added(enumerableset_mock):
+    enumerableset = enumerableset_mock
+
+    for i in range(1,5):
+        await enumerableset.add(i).invoke()
+
+    return enumerableset
+
+
 @pytest.mark.asyncio
 async def test_add(enumerableset_mock):
     enumerableset = enumerableset_mock
-    
-    executed_info = await enumerableset.length().call()
-    assert executed_info.result == (0,)
     
     success = await enumerableset.add(1).invoke()
     assert success.result.success == TRUE
@@ -27,21 +34,30 @@ async def test_add(enumerableset_mock):
     executed_info = await enumerableset.contains(1).call()
     assert executed_info.result == (TRUE,)
 
-    executed_info = await enumerableset.length().call()
-    assert executed_info.result == (1,)
-
 
 @pytest.mark.asyncio
-async def test_remove(enumerableset_mock):
-    enumerableset = enumerableset_mock
-    success = await enumerableset.add(1).invoke()
-    assert success.result.success == TRUE
+async def test_remove(enumerableset_added):
+    enumerableset = enumerableset_added
 
     success = await enumerableset.remove(1).invoke()
     assert success.result.success == TRUE
 
-    executed_info = await enumerableset.length().call()
-    assert executed_info.result == (0,)
+    executed_info = await enumerableset.contains(1).call()
+    assert executed_info.result == (FALSE,)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('value, result', [
+    [1, TRUE],
+    [2, TRUE],
+    [3, TRUE],
+    [4, TRUE],
+    [5, FALSE],
+])
+async def test_contains(enumerableset_added, value, result):
+    enumerableset = enumerableset_added
+    executed_info = await enumerableset.contains(value).call()
+    assert executed_info.result == (result,)
 
 
 @pytest.mark.asyncio
@@ -56,87 +72,48 @@ async def test_remove_not_present(enumerableset_mock):
 
 
 @pytest.mark.asyncio
-async def test_dupe_add(enumerableset_mock):
-    enumerableset = enumerableset_mock
-
-    success = await enumerableset.add(1).invoke()
-    assert success.result.success == TRUE
+async def test_dupe_add(enumerableset_added):
+    enumerableset = enumerableset_added
 
     success = await enumerableset.add(1).invoke()
     assert success.result.success == FALSE
 
-    executed_info = await enumerableset.contains(1).call()
-    assert executed_info.result == (TRUE,)
-
-    executed_info = await enumerableset.length().call()
-    assert executed_info.result == (1,)
-
 
 @pytest.mark.asyncio
-async def test_multi_add(enumerableset_mock):
-    enumerableset = enumerableset_mock
-    
-    for i in [1,3,2]:
-        success = await enumerableset.add(i).invoke()
-        assert success.result.success == TRUE
-
-    for i in [3,2,1]:
-        executed_info = await enumerableset.contains(i).call()
-        assert executed_info.result == (TRUE,)
-
-    executed_info = await enumerableset.length().call()
-    assert executed_info.result == (3,)
-
-
-@pytest.mark.asyncio
-async def test_add_three_remove_one(enumerableset_mock):
-    enumerableset = enumerableset_mock
-
-    for i in [1,3,2]:
-        success = await enumerableset.add(i).invoke()
-        assert success.result.success == TRUE
+@pytest.mark.parametrize('value, result', [
+    [1, TRUE],
+    [2, TRUE],
+    [3, FALSE],
+    [4, TRUE],
+    [5, FALSE],
+])
+async def test_add_three_remove_one(enumerableset_added, value, result):
+    enumerableset = enumerableset_added
 
     success = await enumerableset.remove(3).invoke()
     assert success.result.success == TRUE
 
-    executed_info = await enumerableset.contains(3).call()
-    assert executed_info.result == (FALSE,)
-
-    executed_info = await enumerableset.contains(2).call()
-    assert executed_info.result == (TRUE,)
-
-    executed_info = await enumerableset.contains(1).call()
-    assert executed_info.result == (TRUE,)
-
-    executed_info = await enumerableset.length().call()
-    assert executed_info.result == (2,)
+    executed_info = await enumerableset.contains(value).call()
+    assert executed_info.result == (result,)
 
 
 @pytest.mark.asyncio
-async def test_values(enumerableset_mock):
-    enumerableset = enumerableset_mock
-
-    for i in [1,3,2]:
-        success = await enumerableset.add(i).invoke()
-        assert success.result.success == TRUE
+async def test_values(enumerableset_added):
+    enumerableset = enumerableset_added
 
     executed_info = await enumerableset.values().call()
 
     values = executed_info.result.res
 
-    for i in range(1,4):
+    for i in range(1,5):
         assert i in values
 
-    assert 4 not in values
+    assert 5 not in values
 
 
 @pytest.mark.asyncio
-async def test_values_after_remove(enumerableset_mock):
-    enumerableset = enumerableset_mock
-    
-    for i in [1,3,2,4]:
-        success = await enumerableset.add(i).invoke()
-        assert success.result.success == TRUE
+async def test_values_after_remove(enumerableset_added):
+    enumerableset = enumerableset_added
 
     success = await enumerableset.remove(2).invoke()
     assert success.result.success == TRUE

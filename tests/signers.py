e0a32809c125163dffd940014e916813fa3d78bd
Evolve <Artevolve@yahoo.com>
2022-07-08 11:44:31 +0200

    fix: default max_fee to 1 🔨

--- tests/signers.py
+++ tests/signers.py
@@ -71,10 +71,10 @@ class MockEthSigner():
         self.signer = eth_keys.keys.PrivateKey(private_key)
         self.eth_address = int(self.signer.public_key.to_checksum_address(), 0)
 
-    async def send_transaction(self, account, to, selector_name, calldata, nonce=None, max_fee=0):
+    async def send_transaction(self, account, to, selector_name, calldata, nonce=None, max_fee=1):
         return await self.send_transactions(account, [(to, selector_name, calldata)], nonce, max_fee)
 
-    async def send_transactions(self, account, calls, nonce=None, max_fee=0):
+    async def send_transactions(self, account, calls, nonce=None, max_fee=1):
         if nonce is None:
             execution_info = await account.get_nonce().call()
             nonce, = execution_info.result

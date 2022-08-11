7412f5a3ace9bbfe9d6b0483da6518aacb7fcbb5
Andrew Fleming <fleming-andrew@protonmail.com>
2022-08-11 16:44:34 -0400

    Automate docsite links bump (#420)
    * add README to version script
    
    * consolidate main

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -1,17 +1,17 @@
 import fileinput
+import itertools
 import sys
 from pathlib import Path
 
 CURRENT_VERSION = "v0.3.0"
-ANTORA_PATH = "docs/antora.yml"
+DOC_PATHS = ["docs/antora.yml", "README.md"]
 
 
 def main():
     new_version = str(sys.argv[1])
     path = Path("src")
-    for p in path.glob("**/*.cairo"):
+    for p in itertools.chain(path.glob("**/*.cairo"), DOC_PATHS):
         _update_version(p, new_version)
-    _update_version(ANTORA_PATH, new_version)
     _update_version("scripts/update_version.py", new_version)
 
 
@@ -19,7 +19,7 @@ def _update_version(path, version):
     with fileinput.input(path, inplace=True) as file:
         for line in file:
             old, new = CURRENT_VERSION, version
-            if path == ANTORA_PATH:
+            if path in DOC_PATHS:
                 old = old.strip("v")
                 new = new.strip("v")
             new_line = line.replace(old, new)

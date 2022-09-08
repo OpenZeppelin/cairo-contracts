91c8f78a484dde6d12ba499883ef0320f84393f8
Andrew <fleming-andrew@protonmail.com>
2022-09-08 06:11:27 -0400

    update signers, add mock nile

--- tests/signers.py
+++ tests/signers.py
@@ -1,4 +1,7 @@
-from nile.signer import Signer, from_call_to_call_array, get_transaction_hash
+from starkware.starknet.definitions.general_config import StarknetChainId
+from starkware.starknet.core.os.transaction_hash.transaction_hash import calculate_transaction_hash_common, TransactionHashPrefix
+from starkware.starknet.services.api.gateway.transaction import InvokeFunction
+from starkware.starknet.business_logic.transaction.objects import InternalTransaction, TransactionExecutionInfo
 from utils import to_uint
 import eth_keys
 
@@ -42,20 +45,53 @@ class MockSigner():
     async def send_transaction(self, account, to, selector_name, calldata, nonce=None, max_fee=0):
         return await self.send_transactions(account, [(to, selector_name, calldata)], nonce, max_fee)
 
-    async def send_transactions(self, account, calls, nonce=None, max_fee=0):
-        if nonce is None:
-            execution_info = await account.get_nonce().call()
-            nonce, = execution_info.result
-
+    async def send_transactions(
+        self,
+        account,
+        calls,
+        nonce=None,
+        max_fee=0
+    ) -> TransactionExecutionInfo:
+        # hexify address before passing to from_call_to_call_array
         build_calls = []
         for call in calls:
             build_call = list(call)
             build_call[0] = hex(build_call[0])
             build_calls.append(build_call)
 
-        (call_array, calldata, sig_r, sig_s) = self.signer.sign_transaction(
-            hex(account.contract_address), build_calls, nonce, max_fee)
-        return await account.__execute__(call_array, calldata, nonce).invoke(signature=[sig_r, sig_s])
+        raw_invocation = get_raw_invoke(account, build_calls)
+        state = raw_invocation.state
+
+        if nonce is None:
+            nonce = await state.state.get_nonce_at(account.contract_address)
+
+        transaction_hash = get_transaction_hash(
+            prefix=TransactionHashPrefix.INVOKE,
+            account=account.contract_address,
+            calldata=raw_invocation.calldata,
+            nonce=nonce,
+            max_fee=max_fee
+        )
+
+        # get signature
+        sig_r, sig_s = self.signer.sign(message_hash=transaction_hash)
+
+        # craft invoke and execute tx
+        external_tx = InvokeFunction(
+            contract_address=account.contract_address,
+            calldata=raw_invocation.calldata,
+            entry_point_selector=None,
+            signature=[sig_r, sig_s],
+            max_fee=max_fee,
+            version=TRANSACTION_VERSION,
+            nonce=nonce,
+        )
+
+        tx = InternalTransaction.from_external(
+            external_tx=external_tx, general_config=state.general_config
+        )
+        execution_info = await state.execute_tx(tx=tx)
+        return execution_info
 
 
 class MockEthSigner():
@@ -75,27 +111,122 @@ class MockEthSigner():
         return await self.send_transactions(account, [(to, selector_name, calldata)], nonce, max_fee)
 
     async def send_transactions(self, account, calls, nonce=None, max_fee=0):
-        if nonce is None:
-            execution_info = await account.get_nonce().call()
-            nonce, = execution_info.result
-
         build_calls = []
         for call in calls:
             build_call = list(call)
             build_call[0] = hex(build_call[0])
             build_calls.append(build_call)
 
-        (call_array, calldata) = from_call_to_call_array(build_calls)
-        message_hash = get_transaction_hash(
-            account.contract_address, call_array, calldata, nonce, max_fee
+        raw_invocation = get_raw_invoke(account, build_calls)
+        state = raw_invocation.state
+
+        if nonce is None:
+            nonce = await state.state.get_nonce_at(account.contract_address)
+
+        transaction_hash = get_transaction_hash(
+            prefix=TransactionHashPrefix.INVOKE,
+            account=account.contract_address,
+            calldata=raw_invocation.calldata,
+            nonce=nonce,
+            max_fee=max_fee
         )
 
         signature = self.signer.sign_msg_hash(
-            (message_hash).to_bytes(32, byteorder="big"))
+            (transaction_hash).to_bytes(32, byteorder="big"))
         sig_r = to_uint(signature.r)
         sig_s = to_uint(signature.s)
 
+        external_tx = InvokeFunction(
+            contract_address=account.contract_address,
+            calldata=raw_invocation.calldata,
+            entry_point_selector=None,
+            signature=[signature.v, *sig_r, *sig_s],
+            max_fee=max_fee,
+            version=TRANSACTION_VERSION,
+            nonce=nonce,
+        )
+
+        tx = InternalTransaction.from_external(
+            external_tx=external_tx, general_config=state.general_config
+        )
+
+        execution_info = await state.execute_tx(tx=tx)
         # the hash and signature are returned for other tests to use
-        return await account.__execute__(call_array, calldata, nonce).invoke(
-            signature=[signature.v, *sig_r, *sig_s]
-        ), message_hash, [signature.v, *sig_r, *sig_s]
+        return execution_info, transaction_hash, [signature.v, *sig_r, *sig_s]
+
+
+###################### TMP NILE ######################
+
+
+from starkware.starknet.definitions.general_config import StarknetChainId
+from starkware.starknet.core.os.transaction_hash.transaction_hash import calculate_transaction_hash_common, TransactionHashPrefix
+from starkware.crypto.signature.signature import private_to_stark_key, sign
+from starkware.starknet.public.abi import get_selector_from_name
+
+
+TRANSACTION_VERSION = 1
+
+class Signer:
+    """Utility for signing transactions for an Account on Starknet."""
+
+    def __init__(self, private_key):
+        """Construct a Signer object. Takes a private key."""
+        self.private_key = private_key
+        self.public_key = private_to_stark_key(private_key)
+
+    def sign(self, message_hash):
+        """Sign a message hash."""
+        return sign(msg_hash=message_hash, priv_key=self.private_key)
+
+    def sign_transaction(self, sender, calls, nonce, max_fee):
+        raw_invocation = get_raw_invoke(sender, calls)
+
+        transaction_hash = get_transaction_hash(
+            prefix=TransactionHashPrefix.INVOKE,
+            account=sender.contract_address,
+            calldata=raw_invocation.calldata,
+            nonce=nonce,
+            max_fee=max_fee
+        )
+
+        sig_r, sig_s = self.sign(message_hash=transaction_hash)
+        return (sig_r, sig_s)
+
+
+# Auxiliary functions
+
+
+def from_call_to_call_array(calls):
+    """Transform from Call to CallArray."""
+    call_array = []
+    calldata = []
+    for _, call in enumerate(calls):
+        assert len(call) == 3, "Invalid call parameters"
+        entry = (
+            int(call[0], 16),
+            get_selector_from_name(call[1]),
+            len(calldata),
+            len(call[2]),
+        )
+        call_array.append(entry)
+        calldata.extend(call[2])
+    return (call_array, calldata)
+
+
+def get_transaction_hash(prefix, account, calldata, nonce, max_fee):
+    return calculate_transaction_hash_common(
+            tx_hash_prefix=prefix,
+            version=TRANSACTION_VERSION,
+            contract_address=account,
+            entry_point_selector=0,
+            calldata=calldata,
+            max_fee=max_fee,
+            chain_id=StarknetChainId.TESTNET.value,
+            additional_data=[nonce],
+    )
+
+
+def get_raw_invoke(sender, calls):
+    call_array, calldata = from_call_to_call_array(calls)
+    raw_invocation = sender.__execute__(call_array, calldata)
+    return raw_invocation

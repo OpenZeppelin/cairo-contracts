7d84b826969277b19220a0549ba2b8c6eddd2878
Eric Nordelo <eric.nordelo39@gmail.com>
2022-10-24 00:49:35 +0200

    feat: move get_cairo_path

--- tests/token/erc20/ERC20BaseSuite.py
+++ tests/token/erc20/ERC20BaseSuite.py
@@ -4,10 +4,10 @@ import os
 from signers import MockSigner
 from nile.utils import (
     to_uint, add_uint, sub_uint, str_to_felt, MAX_UINT256, ZERO_ADDRESS,
-    INVALID_UINT256, TRUE, assert_revert, get_cairo_path
+    INVALID_UINT256, TRUE, assert_revert
 )
 from utils import (
-    assert_event_emitted, assert_events_emitted, contract_path, State
+    assert_event_emitted, assert_events_emitted, contract_path, State, get_cairo_path
 )
 
 

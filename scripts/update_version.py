e0e07555b319d8131a35259b55269ba7b19e744e
Eric Nordelo <eric.nordelo39@gmail.com>
2022-12-18 07:34:48 +0100

    feat: update adoc files (#535)

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -4,13 +4,16 @@ import sys
 from pathlib import Path
 
 CURRENT_VERSION = "v0.5.1"
-DOC_PATHS = ["docs/antora.yml", "README.md"]
+OTHER_PATHS = ["docs/antora.yml", "README.md"]
 
 
 def main():
     new_version = str(sys.argv[1])
-    path = Path("src")
-    for p in itertools.chain(path.glob("**/*.cairo"), DOC_PATHS):
+    src_path = Path("src")
+    docs_path = Path("docs")
+    for p in itertools.chain(
+        src_path.glob("**/*.cairo"),
+        docs_path.glob("**/*.adoc"), OTHER_PATHS):
         _update_version(p, new_version)
     _update_version("scripts/update_version.py", new_version)
 
@@ -19,7 +22,7 @@ def _update_version(path, version):
     with fileinput.input(path, inplace=True) as file:
         for line in file:
             old, new = CURRENT_VERSION, version
-            if path in DOC_PATHS:
+            if path in OTHER_PATHS:
                 old = old.strip("v")
                 new = new.strip("v")
             new_line = line.replace(old, new)

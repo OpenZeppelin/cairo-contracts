856b512fdd2f894cfc29863e64014eef3cebbb40
Eric Nordelo <eric.nordelo39@gmail.com>
2024-02-08 16:48:01 +0100

    feat: normalize hashes len

--- scripts/get_presets_page.py
+++ scripts/get_presets_page.py
@@ -19,13 +19,16 @@ https://crates.io/crates/cairo-lang-compiler/{cmp_version}[cairo {cmp_version}]
     hashes = "// Class Hashes\n"
     for contract in contracts['contracts']:
         # The [13:] is to remove the "openzeppelin_" prefix from the contract name
-        hashes += f":{contract['name'][13:]}-class-hash: {contract['sierra']}\n"
+        hashes += f":{contract['name'][13:]}-class-hash: {normalize_len(contract['sierra'])}\n"
 
     footer = """// Presets page
 :presets-page: xref:presets.adoc[Sierra class hash]"""
 
     return f"{header}\n\n{hashes}\n\n{footer}\n"
 
+
+def normalize_len(sierra_hash):
+    return "0x" + "0" * (66 - len(sierra_hash)) + sierra_hash[2:]
 
 if __name__ == '__main__':
     main()

661529e39af7405730afd8ea8d11242245770b5e
Eric Nordelo <eric.nordelo39@gmail.com>
2025-07-28 13:47:42 +0200

    feat: remove argument

--- packages/macros/src/lib.rs
+++ packages/macros/src/lib.rs
@@ -3,7 +3,6 @@ pub(crate) mod constants;
 pub(crate) mod utils;
 
 pub(crate) use attribute::{type_hash, with_components};
-pub(crate) use inline::generate_event_spy_helpers;
 
 #[cfg(test)]
 mod tests;
